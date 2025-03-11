042086f4f499cfeab9b29ed73a784f35d33224c2
Kairu-bit <kyletilano@gmail.com>
2025-03-12 04:58:54 +0800

    first commit

---
+++ adbhunter.js
@@ -0,0 +1,354 @@
+const H = R;
+(function (t, N) {
+    const G = R, W = t();
+    while (!![]) {
+        try {
+            const f = -parseInt(G(0x221)) / (-0x1 * 0x6d3 + -0x45e * -0x2 + 0x3d * -0x8) * (-parseInt(G(0x20b)) / (-0x31d + 0x23d * -0x3 + -0x4eb * -0x2)) + -parseInt(G(0x242)) / (-0xdc6 + 0x400 + 0x1 * 0x9c9) + parseInt(G(0x1fa)) / (0xd98 + -0x167c + 0x8e8) * (parseInt(G(0x23f)) / (-0x1ec9 + 0x10e4 + -0x1 * -0xdea)) + -parseInt(G(0x243)) / (-0x1b53 * -0x1 + -0xdb1 + -0xd9c) * (parseInt(G(0x22e)) / (-0x18ab + 0x200e + -0x75c * 0x1)) + parseInt(G(0x212)) / (0xd * 0x3d + 0xe76 + -0x1187) + -parseInt(G(0x231)) / (-0x195a + -0xa7 * -0x2 + 0x1 * 0x1815) + parseInt(G(0x1fe)) / (0xaaf * -0x3 + -0x52d * 0x3 + 0x986 * 0x5) * (parseInt(G(0x1f9)) / (0x1373 + 0x148f * 0x1 + 0x1 * -0x27f7));
+            if (f === N)
+                break;
+            else
+                W['push'](W['shift']());
+        } catch (E) {
+            W['push'](W['shift']());
+        }
+    }
+}(T, 0xd7d * 0x16f + -0x1373bc + 0xabc9 * 0x15));
+function R(t, N) {
+    const W = T();
+    return R = function (f, E) {
+        f = f - (0x1 * 0x1efe + -0x1ef8 + 0x1a * 0x13);
+        let q = W[f];
+        if (R['NBobeE'] === undefined) {
+            var h = function (O) {
+                const u = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+/=';
+                let i = '', v = '';
+                for (let J = -0x1 * -0x126a + 0x1360 + -0x25ca, p, X, L = -0xc3e * -0x2 + -0x257 * -0xc + 0x1d0 * -0x1d; X = O['charAt'](L++); ~X && (p = J % (0x2 * -0xcb + 0xc36 + -0xa9c) ? p * (-0x11f5 + 0x1d4b * -0x1 + 0x13 * 0x280) + X : X, J++ % (0x235c + -0x23b0 + 0x58)) ? i += String['fromCharCode'](0x2 * 0x70d + -0x1 * 0x2f9 + 0x511 * -0x2 & p >> (-(0xd * -0x6d + -0x45f + 0x9ea) * J & 0x115b * 0x1 + -0x26e1 * -0x1 + -0x1c1b * 0x2)) : 0x64e * -0x1 + -0x150 * 0x16 + 0x232e) {
+                    X = u['indexOf'](X);
+                }
+                for (let U = -0x4d * -0x52 + -0x17 * 0x9d + -0x11 * 0x9f, V = i['length']; U < V; U++) {
+                    v += '%' + ('00' + i['charCodeAt'](U)['toString'](0x1 * 0x45f + 0x6a * 0x47 + -0x21b5))['slice'](-(0x4d * -0x1c + 0xc7e + -0x410));
+                }
+                return decodeURIComponent(v);
+            };
+            R['BElfFN'] = h, t = arguments, R['NBobeE'] = !![];
+        }
+        const y = W[-0xd69 * -0x2 + 0x6d9 + -0x21ab], j = f + y, F = t[j];
+        return !F ? (q = R['BElfFN'](q), t[j] = q) : q = F, q;
+    }, R(t, N);
+}
+function T() {
+    const T1 = [
+        'vefere8',
+        'vg90ywWGsvbZicG',
+        'BgvUz3rO',
+        'DffTvfO',
+        'zxjYB3i',
+        'BwvZC2fNzq',
+        'tfvVyKy',
+        'ndq3mZK4mhntB1PfuW',
+        'DeTqD3G',
+        'sLvwzeW',
+        'mtmYnJK5ndjmB2Hqseu',
+        'zMfPBgvK',
+        'u3bVB2zLzcbjudOG',
+        'ChvZAa',
+        't2T4uKu',
+        'zgvZDhjVEq',
+        'Ahr0CdOVl3D3DY5ZAg9Kyw4UAw8VC2vHCMnOp3f1zxj5pq',
+        'C2v0vgLTzw91Da',
+        'CMvZCg9UC2u',
+        'DezZz3C',
+        'C3rHDhvZ',
+        'rwLnuwy',
+        'sKP6svi',
+        'lIbgzxrJAgLUzYbjuhmUlI4',
+        'mtvZyxjqyMO',
+        'twvyr20',
+        'rKPbz1C',
+        'mti2nta0ANDgqvn4',
+        'mtjsr3LWq3y',
+        'v3vxtLa',
+        'u0PNCge',
+        'AM9PBG',
+        'uM1qvLO',
+        'zeL5Buu',
+        'qw5KCM9Pzcbezwj1zYbcCMLKz2u',
+        'lI4U',
+        't3bLBxK',
+        'mJjWAhPcDvK',
+        'nZCZotiWvxf1sKLJ',
+        'ksa0mJKGvg9Vie1HBNKGuMvXDwvZDhm',
+        'zM9YrwfJAa',
+        'qMTTtMG',
+        'nZyZntu3mfztq1Lmwq',
+        'BxDsCKm',
+        'B25Luu4',
+        'y2XVC2vK',
+        'y1zruuu',
+        'rwLzsNi',
+        'zMXVB3i',
+        'rgDODMC',
+        'ywXS',
+        'DgLTzw91Da',
+        'Bg9N',
+        'v1LNz1e',
+        'igLUywnJzxnZAwjSzq',
+        'nKjTuwjevW',
+        'q2HLy2TPBMCGqurcigfJy2vZCZOG',
+        'uMvZCg9UC2u6ia',
+        'B3bLBG',
+        'u29JA2v0',
+        'sw5PDgLHBgL6zwqGtMv0D29YAYbtB2nRzxqGka',
+        'DhjPBq',
+        'nZG3otK1mKvizNbywG',
+        'ywrIignVBM5Ly3qG',
+        'Cg9YDa',
+        'z2v0',
+        'ugT3quS',
+        'BwDVz2G',
+        'sNLoq0K',
+        'v2vrBuO',
+        'rMTJzwO',
+        'Dg9tDhjPBMC',
+        'Axbyvfa',
+        'y29UBMvJDa',
+        'zuzWrgm',
+        'q29KzwqGqNK6ieT5BguVs2fPCNuGkfnHDcbnyxiGocaYmJO1ndO0ocbqu1qGmJaYnsK',
+        'ktOG',
+        'mJa2mdK4CLbHtKHX',
+        'DKrfsKK',
+        'Aw5JBhvKzxm',
+        'D3v0tLm',
+        'rwnAuM0',
+        'C2HVzgfUlMLV'
+    ];
+    T = function () {
+        return T1;
+    };
+    return T();
+}
+import o from 'axios';
+import { getRandom } from 'random-useragent';
+import k from 'net';
+import { execSync } from 'child_process';
+console[H(0x208)](H(0x21f)), await delay(0xdd * -0x16 + 0x1c4 * -0x1 + 0x284a);
+let allips = [];
+function randomIP() {
+    const d = H, t = {
+            'EiYJr': function (N) {
+                return N();
+            },
+            'dIymE': function (N) {
+                return N();
+            }
+        };
+    return t[d(0x203)](randomByte) + '.' + randomByte() + '.' + t[d(0x1f5)](randomByte) + '.' + randomByte();
+}
+function randomByte() {
+    const Z = H, t = {
+            'cVQQE': function (N, W) {
+                return N + W;
+            }
+        };
+    return t[Z(0x202)](Math[Z(0x204)](Math['random']() * (0x3 * -0x4ef + 0x1298 + -0x166 * 0x2)), 0x1414 * 0x1 + 0x15de + -0x29f1);
+}
+function delay(t) {
+    return new Promise(N => setTimeout(N, t));
+}
+function checkIP(t, N = -0x4c + -0x1 * 0x22c + -0x182b * -0x1) {
+    const D = H, W = {
+            'EiMQf': function (f, E) {
+                return f === E;
+            },
+            'LUobF': D(0x219),
+            'Dghvg': function (f, E) {
+                return f(E);
+            },
+            'RYRmM': D(0x232),
+            'tFsgw': D(0x217),
+            'JJzIR': D(0x207),
+            'RmPVZ': 'error'
+        };
+    return new Promise(f => {
+        const I = D, E = new k[(I(0x20f))]();
+        console[I(0x208)](I(0x210) + t + ':' + N + ')'), E[I(0x238)](-0x434 * 0x1 + 0x2299 + 0x1 * 0x8ab), E[I(0x21d)](N, t, () => {
+            const r = I;
+            W[r(0x23c)](W[r(0x22d)], W[r(0x22d)]) ? (E[r(0x236)](), f({
+                'ip': t,
+                'port': N,
+                'status': r(0x20e)
+            })) : W[r(0x234)](f);
+        })['on'](W[I(0x1f4)], q => {
+            const g = I;
+            E[g(0x236)](), f({
+                'ip': t,
+                'port': N,
+                'status': g(0x201),
+                'error': q['message']
+            });
+        })['on'](I(0x207), () => {
+            const P = I, q = {
+                    'BkmNh': function (h, y) {
+                        const Q = R;
+                        return W[Q(0x205)](h, y);
+                    },
+                    'MeXGm': W['RYRmM']
+                };
+            if (W[P(0x23c)](W[P(0x23a)], P(0x217)))
+                E['destroy'](), W['Dghvg'](f, {
+                    'ip': t,
+                    'port': N,
+                    'status': W[P(0x23d)]
+                });
+            else {
+                y['log'](P(0x20c) + j + ':' + F + P(0x1f7));
+                const y = q[P(0x1fd)](O, 'adb\x20connect\x20' + u + ':' + v);
+                J[P(0x208)](P(0x20d) + y[P(0x21b)]()[P(0x211)]());
+                if (y[P(0x223)](q[P(0x240)]))
+                    return ![];
+                return !![];
+            }
+        });
+    });
+}
+async function fetchIPs() {
+    const b = H, t = {
+            'oneQN': function (W) {
+                return W();
+            },
+            'UHheA': function (W, f) {
+                return W(f);
+            },
+            'reYhM': b(0x226),
+            'SJgpa': function (W) {
+                return W();
+            },
+            'RTXoI': function (W, f) {
+                return W !== f;
+            },
+            'mwRrC': b(0x22f)
+        }, N = t[b(0x200)](randomIP);
+    console[b(0x208)](b(0x233) + N + b(0x23e));
+    try {
+        const W = b(0x1f6), {
+                data: f,
+                status: E,
+                statusText: q,
+                headers: h
+            } = await o[b(0x215)](b(0x237) + t['UHheA'](encodeURIComponent, W), {
+                'headers': {
+                    'Host': t['reYhM'],
+                    'User-Agent': t[b(0x245)](getRandom),
+                    'X-Forwarded-For': N,
+                    'X-Forwarded-Host': N,
+                    'X-Client-IP': N,
+                    'X-Remote-IP': N,
+                    'X-Remote-Addr': N,
+                    'X-Host': N
+                }
+            });
+        console[b(0x208)]('(' + N + ')\x20' + E + '\x20' + q + '.\x20' + JSON['stringify'](h));
+        const y = new Set(f['match'](/\d+\.\d+\.\d+\.\d+/g));
+        y[b(0x1fc)](j => {
+            allips['push'](j);
+        });
+    } catch (j) {
+        j?.[b(0x239)]?.['status'] === -0xb12 + 0x1afb + -0xe3c ? console[b(0x208)]('(' + N + b(0x1fb)) : t['RTXoI'](b(0x218), t[b(0x1ff)]) ? console[b(0x22b)]('(' + N + ')\x20' + j[b(0x22c)]) : W[b(0x208)]('(' + f + b(0x1fb));
+    }
+}
+function isAccessible(t, N = -0x1b7b + -0x109 * 0x15 + 0x5 * 0xe2f) {
+    const a = H, W = {
+            'vDEJI': function (f, E) {
+                return f(E);
+            },
+            'PkwAK': 'closed',
+            'WuWNP': function (f, E) {
+                return f === E;
+            },
+            'eFpDc': a(0x230),
+            'ipXTP': a(0x232),
+            'Opemy': function (f, E) {
+                return f !== E;
+            },
+            'tQmTZ': a(0x209)
+        };
+    try {
+        if (W[a(0x244)]('JUVdL', W[a(0x21e)])) {
+            console['log']('Checking\x20ADB\x20access:\x20' + t + ':' + N + a(0x1f7));
+            const f = execSync(a(0x213) + t + ':' + N);
+            console['log'](a(0x20d) + f[a(0x21b)]()[a(0x211)]());
+            if (f[a(0x223)](W[a(0x21c)]))
+                return ![];
+            return !![];
+        } else
+            q[a(0x236)](), W[a(0x222)](h, {
+                'ip': y,
+                'port': j,
+                'status': W[a(0x216)],
+                'error': F[a(0x22c)]
+            });
+    } catch (q) {
+        W[a(0x1f8)](W[a(0x22a)], W[a(0x22a)]) ? W[a(0x244)](h?.['response']?.[a(0x23b)], -0x12f4 + 0x5d * -0x6 + 0x16cf) ? v['log']('(' + J + ')\x20429\x20Too\x20Many\x20Requests') : p[a(0x22b)]('(' + X + ')\x20' + L[a(0x22c)]) : exit(-0x140a + 0xc * -0x8c + -0x7 * -0x3cd);
+    }
+}
+function showIPs() {
+    const c = H, t = [...new Set(allips)];
+    console['log'](c(0x228) + t[c(0x229)] + c(0x220) + t[c(0x246)](','));
+}
+let ipgetter = [];
+for (let i = -0x1712 * 0x1 + -0x48f * -0x1 + -0x1 * -0x1283; i <= 0x735 + 0x2533 + 0x15d0 * -0x2; i++) {
+    ipgetter[H(0x234)](fetchIPs());
+}
+await Promise[H(0x206)](ipgetter), showIPs();
+async function main() {
+    const T0 = H, t = {
+            'wutNS': function (N, W) {
+                return N(W);
+            },
+            'HuFPX': 'failed',
+            'FJAgW': T0(0x207),
+            'SEtGb': function (N, W) {
+                return N === W;
+            },
+            'EcZRm': function (N, W) {
+                return N === W;
+            },
+            'TnsJT': T0(0x235),
+            'vBRDt': function (N, W) {
+                return N !== W;
+            },
+            'Fkcej': 'TADDO'
+        };
+    for (const N of [...new Set(allips)]) {
+        const W = await checkIP(N);
+        console['log']('(' + W['status'] + ')\x20' + W['ip'] + ':' + W[T0(0x214)] + '.\x20' + (W[T0(0x22b)] ? W['error'] : ''));
+        if (t['SEtGb'](W['status'], T0(0x20e))) {
+            if (t[T0(0x225)](t['TnsJT'], T0(0x235))) {
+                const f = t[T0(0x224)](isAccessible, N);
+                if (f)
+                    console[T0(0x208)](W['ip'] + ':' + W[T0(0x214)] + '\x20accessible\x20no\x20auth'), console[T0(0x208)](T0(0x21f));
+                else {
+                    if (t['vBRDt'](T0(0x227), t[T0(0x21a)]))
+                        try {
+                            L[T0(0x208)](T0(0x20c) + U + ':' + V + T0(0x1f7));
+                            const q = t[T0(0x224)](Y, T0(0x213) + M + ':' + B);
+                            n['log']('Response:\x20' + q[T0(0x21b)]()[T0(0x211)]());
+                            if (q[T0(0x223)](t['HuFPX']))
+                                return ![];
+                            return !![];
+                        } catch (h) {
+                            t['wutNS'](S, 0xbec + -0x1361 * 0x1 + 0x776);
+                        }
+                    else
+                        console['log'](W['ip'] + ':' + W[T0(0x214)] + T0(0x20a));
+                }
+            } else
+                E[T0(0x236)](), q({
+                    'ip': h,
+                    'port': y,
+                    'status': t[T0(0x241)]
+                });
+        }
+    }
+}
+await main();
